d2939693f4fb7e0ac5b7206aa5fc9628d341fc3b
VishalParwani76 <vishalparwani1970@gmail.com>
2026-08-02 10:55:33 +0500

    Complete PSet 1 Home Federal Savings Bank

---
+++ pset1/bank.py
@@ -0,0 +1,11 @@
+greeting = input("Greeting: ")
+
+greeting = greeting.strip()
+greeting = greeting.lower()
+
+if greeting.startswith("hello"):
+    print("$0")
+elif greeting.startswith("h"):
+    print("$20")
+else:
+    print("$100")
