9f990876929b4fd5b7b3ce13bd4b4400c5944e04
VishalParwani76 <vishalparwani1970@gmail.com>
2026-08-10 01:15:27 +0500

    Complete PSet 3 Grocery List

---
+++ pset3/grocery.py
@@ -0,0 +1,18 @@
+grocery = {}
+
+while True:
+    try:
+        item = input("").upper()
+
+        if item in grocery:
+            grocery[item] += 1
+
+        else:
+            grocery[item] = 1
+
+    except EOFError:
+        break;
+
+
+for item in sorted(grocery):
+    print(grocery[item], item)
