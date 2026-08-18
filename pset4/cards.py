0e7ce628a7b44fe767b7f1ccc49d3a328504b786
VishalParwani76 <vishalparwani1970@gmail.com>
2026-08-18 18:35:03 +0500

    pset4: Add random.seed() inside  cards.py

--- pset4/cards.py
+++ pset4/cards.py
@@ -4,7 +4,7 @@ cards = ["jack", "queen", "king"]
 
 def main():
     # Use random.seed(0) for debugging 
-    print(random.choices(cards, weights=[75, 20, 5], k=2))
+     print(random.choices(cards, weights=[75, 20, 5], k=2))
 
 
 main()
